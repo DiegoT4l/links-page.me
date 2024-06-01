84ac70c3b0b39d0d58ff974b80ca211478a7c1df
DiegoT4l <128425675+DiegoT4l@users.noreply.github.com>
2024-06-01 17:54:11 -0600

    chore: optimize Hero component and update fonts
    - Optimize Hero component by using a fixed height for the background video and applying a custom gradient overlay.
    - Update font URLs in fonts.css to use the correct format for truetype fonts.
    - Update the body background color in Layout.astro to a specific hex value.
    - Add Avatar component to the main section in index.astro.
    
    Fixes #123

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class', 
+	darkMode: 'class',
 	theme: {
 		extend: {
 			colors: {
 				customBackgroundStart: 'rgba(250,250,250,0)',
 				customBackgroundEnd: 'rgb(9, 9, 11)',
+				'custom-transparent': 'rgba(250,250,250,0)',
+				'custom-black': 'rgb(9, 9, 11)',
 			},
-			gradientPercentages: {
-				'0': '0%',
-				'95': '95.17178922086148%',
+			backgroundImage: {
+				'custom-gradient': 'linear-gradient(180deg, rgba(250, 250, 250, 0) 0%, rgb(9, 9, 11) 95%, rgb(9, 9, 11))',
 			},
 			fontFamily: {
 				'rounabold': ['rounabold'],
