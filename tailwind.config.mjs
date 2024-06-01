dbdb41b25f8256600bcb108ef7ce4a9b2cb66e3b
DiegoT4l <128425675+DiegoT4l@users.noreply.github.com>
2024-06-01 14:33:00 -0600

    Feat: added new font type

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,19 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class', 
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				customBackgroundStart: 'rgba(250,250,250,0)',
+				customBackgroundEnd: 'rgb(9, 9, 11)',
+			},
+			gradientPercentages: {
+				'0': '0%',
+				'95': '95.17178922086148%',
+			},
+			fontFamily: {
+				'rounabold': ['rounabold'],
+			}
+		},
 	},
 	plugins: [],
 }
